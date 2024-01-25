aa0c90d4b6ce1ad59d8f49a543843ff9d54885bf
caritasem <caritasem@qq.com>
2024-01-25 19:08:49 +0800

    012502 change default port

--- src/train_web.py
+++ src/train_web.py
@@ -4,7 +4,8 @@ from llmtuner import create_ui
 def main():
     demo = create_ui()
     demo.queue()
-    demo.launch(server_name="0.0.0.0", share=False, inbrowser=True)
+    demo.launch(server_name="0.0.0.0", share=False,
+                inbrowser=True, server_port=6006)
 
 
 if __name__ == "__main__":
